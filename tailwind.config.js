f99996cf18b27eb38ffef30e5bc9b1c7f6c22368
veronicaddiaz7 <veronica.ddiaz7@gmail.com>
2023-01-09 07:44:10 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     extend: {
      backgroundImage:{
-      'wrinkledpaper':"url('.src/components/wrinkledpaperbackground.webp')",
+      'wrinkledpaper':"url('./src/components/wrinkledpaperbackground.webp')",
       'yellowruledpaper':"url('./components/yellowruledpaper.webp')",
      }
     },
